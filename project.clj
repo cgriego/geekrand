57a76c30c72b9656eac62046fdd774e4b219acff
Chris Griego <cgriego@gmail.com>
2013-03-10 16:16:57 -0500

    Upgrade Clojure

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject geekrand "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.6.5"]
                  [lib-noir "0.4.9"]
                  [compojure "1.1.5"]
