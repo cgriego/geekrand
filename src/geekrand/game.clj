3a98d9087b885d985f22b84172bcb51a06682bc5
Chris Griego <cgriego@gmail.com>
2013-03-12 23:39:29 -0500

    Use thumbnail and extract games logic to game ns

--- src/geekrand/game.clj
+++ src/geekrand/game.clj
@@ -1,6 +1,28 @@
-(ns geekrand.game)
+(ns geekrand.game
+  (:require [geekrand.client :as client]
+            [clojure.xml]
+            [clojure.zip]
+            [clojure.data.zip.xml]))
 
-(defrecord Game [name objectid image])
+(defrecord Game [name objectid image thumbnail])
 
 (defn game-url [game]
   (str "http://boardgamegeek.com/boardgame/" (:objectid game)))
+
+(defn get-games [username]
+  (let [xml (-> (client/get-collection-stream username) clojure.xml/parse clojure.zip/xml-zip)
+        items (clojure.data.zip.xml/xml-> xml :item)]
+    (map (fn [item]
+      (map->Game {
+        :objectid  (clojure.data.zip.xml/xml1-> item (clojure.data.zip.xml/attr :objectid))
+        :name      (clojure.data.zip.xml/xml1-> item :name clojure.data.zip.xml/text)
+        :image     (clojure.data.zip.xml/xml1-> item :image clojure.data.zip.xml/text)
+        :thumbnail (clojure.data.zip.xml/xml1-> item :thumbnail clojure.data.zip.xml/text)
+      })
+    ) items)))
+
+(defn get-random-games [amount username]
+  (take amount (shuffle (get-games username))))
+
+(defn get-random-game [username]
+  (rand-nth (get-games username)))
--- src/geekrand/routes/home.clj
+++ src/geekrand/routes/home.clj
@@ -1,30 +1,12 @@
 (ns geekrand.routes.home
   (:use compojure.core hiccup.element geekrand.game)
   (:require [geekrand.views.layout :as layout]
-            [geekrand.util :as util]
-            [geekrand.client :as client]
-            [clojure.xml]
-            [clojure.zip]
-            [clojure.data.zip.xml]))
-
-(defn get-games [username]
-  (let [xml (-> (client/get-collection-stream username) clojure.xml/parse clojure.zip/xml-zip)
-        items (clojure.data.zip.xml/xml-> xml :item)]
-    (map (fn [item]
-      (map->Game {
-        :objectid (clojure.data.zip.xml/xml1-> item (clojure.data.zip.xml/attr :objectid))
-        :name     (clojure.data.zip.xml/xml1-> item :name clojure.data.zip.xml/text)
-        :image    (clojure.data.zip.xml/xml1-> item :image clojure.data.zip.xml/text)
-      })
-    ) items)))
-
-(defn get-random-game [username]
-  (rand-nth (get-games username)))
+            [geekrand.util :as util]))
 
 (defn home-page []
   (let [game (get-random-game "DGM Library")]
     (layout/common
-      [:h1 (link-to (game-url game) (:name game) [:br] (image (:image game) ""))])))
+      [:h1 (link-to (game-url game) (:name game) [:br] (image (:thumbnail game) ""))])))
 
 (defroutes home-routes
   (GET "/" [] (home-page)))
