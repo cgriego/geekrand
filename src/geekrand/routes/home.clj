37cd3d71b0a09ae2ca481466261f9d2daa1fa863
Chris Griego <cgriego@gmail.com>
2013-04-20 00:02:42 -0500

    Instead of a default user, present a linked list of example users

--- src/geekrand/routes/home.clj
+++ src/geekrand/routes/home.clj
@@ -3,17 +3,23 @@
   (:require [geekrand.views.layout :as layout]
             [geekrand.util :as util]))
 
-(defn home-page [username-param]
-  (let [username (if-not (nil? username-param) username-param "DGM Library")
-        game (random-game username)]
-    (layout/common
-      (form-to {:style "margin: 10px 0;"} [:get "/"]
-        [:div {:class "input-append"}
-          (text-field "username" username)
-          [:button {:type "submit" :class "btn"} "Randomize"]])
-      (if (nil? game)
-        [:p {:class "lead"} [:strong "You don't have any games!"]]
-        (link-to (game-url game) [:h1 (:name game)] (image (:thumbnail game) ""))))))
+(defn home-page [username]
+  (layout/common
+    (form-to {:style "margin: 10px 0;"} [:get "/"]
+      [:div.input-append
+        (text-field {:placeholder "BGG Username"} "username" username)
+        [:button.btn {:type "submit"} "Randomize"]])
+    (if (empty? username)
+      (list
+        [:p.lead [:strong "Example Users"]]
+        [:ul
+          [:li (link-to "/?username=DGM+Library" "DGM Library")]
+          [:li (link-to "/?username=TomVasel" "TomVasel")]
+          [:li (link-to "/?username=cgriego" "cgriego")]])
+      (let [game (random-game username)]
+        (if (nil? game)
+          [:p.lead [:strong "You don't have any games!"]]
+          (link-to (game-url game) [:h1 (:name game)] (image (:thumbnail game) "")))))))
 
 (defroutes home-routes
   (GET "/" [username] (home-page username)))
